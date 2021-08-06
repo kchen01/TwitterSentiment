543e0d79b2562b63524c8a06e3685743bfd9256c
Kevin Chen <usa.kevin@gmail.com>
2021-08-06 12:14:14 -0500

    Add files via upload

---
+++ codes.py
@@ -0,0 +1,4 @@
+consumer_key = '7vSpMULk431Fg5iqum9H9JF7n'
+consumer_secret = 'YwbSJ59eSPPpGKvgMGVQ8YwgROzwd5mhrtnBPZ1nqNMfCaXPJA'
+access_token_key = '397566240-yHHX2E4eashGUIAQfB9DEeIy03yRdLm551MzPnTv'
+access_token_secret = 'vzUwg3ODFXISBHf4kgIvgqGeSiFJ18BdEgB0kvnbRsO9L'
---
+++ watsonTest.py
@@ -0,0 +1,17 @@
+apikey = 'Ufh0HLmiJsrRftdDNLxKX-Y4wsk1529Kqy5Rz4I5wma1'
+url = 'https://api.us-south.tone-analyzer.watson.cloud.ibm.com/instances/fe6c2f19-6c9b-4406-a09b-abb62fd53f65'
+
+from ibm_watson import ToneAnalyzerV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+import json
+
+authenticator = IAMAuthenticator(apikey)
+ta = ToneAnalyzerV3(version='2017-09-21', authenticator=authenticator)
+ta.set_service_url(url)
+
+res = ta.tone('I cried watching YOU. I feel so lucky to have gotten to watch you all these years, but this week was a lesson in emotional intelligence and resilience. We all learned from you. Thank you.').get_result()
+
+for tone in res["document_tone"]["tones"]:
+    print(f'{tone["tone_name"]} with a score of {tone["score"]}')
+
+#print(json.dumps(res, indent = 4))
