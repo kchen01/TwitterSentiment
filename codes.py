4d1556cbc95ab5757fb4a0891684782a05518153
Kevin Chen <usa.kevin@gmail.com>
2021-08-15 17:26:14 -0500

    Delete codes.py

--- codes.py
+++
@@ -1,7 +0,0 @@
-consumer_key = '7vSpMULk431Fg5iqum9H9JF7n'
-consumer_secret = 'YwbSJ59eSPPpGKvgMGVQ8YwgROzwd5mhrtnBPZ1nqNMfCaXPJA'
-access_token_key = '397566240-yHHX2E4eashGUIAQfB9DEeIy03yRdLm551MzPnTv'
-access_token_secret = 'vzUwg3ODFXISBHf4kgIvgqGeSiFJ18BdEgB0kvnbRsO9L'
-apikey = 'Ufh0HLmiJsrRftdDNLxKX-Y4wsk1529Kqy5Rz4I5wma1'
-url = 'https://api.us-south.tone-analyzer.watson.cloud.ibm.com/instances/fe6c2f19-6c9b-4406-a09b-abb62fd53f65'
-token = 'AAAAAAAAAAAAAAAAAAAAAKNQSQEAAAAATujE1XvHqsQF5RqonaaDIYeyuTA%3DisMmy77pmtLnagg2P4QNAixguAt2GEOeSX3w7WJ416FgeEMIIv'
